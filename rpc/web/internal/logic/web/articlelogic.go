b2c4dfb769fa1d27d98705f2c7bc7adbd7eeafb9
leixiaotian <tian-lei-happy@163.com>
2024-09-21 20:45:50 +0800

    debug

--- rpc/web/internal/logic/web/articlelogic.go
+++ rpc/web/internal/logic/web/articlelogic.go
@@ -3,10 +3,7 @@ package weblogic
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"github.com/zeromicro/go-zero/core/logc"
-	"go.mongodb.org/mongo-driver/mongo"
-	model "lxtian-blog/rpc/web/model/mongo"
 	"lxtian-blog/rpc/web/model/mysql"
 
 	"lxtian-blog/rpc/web/internal/svc"
@@ -35,7 +32,7 @@ func (l *ArticleLogic) Article(in *web.ArticleReq) (*web.ArticleResp, error) {
 	var article map[string]interface{}
 	err := l.svcCtx.DB.
 		Model(&mysql.TxyArticle{}).
-		Select("id,title,author,description,keywords,content,cid,_id").
+		Select("id,title,author,description,keywords,content,cid").
 		Where(where).
 		Debug().
 		Find(&article).Error
@@ -45,29 +42,28 @@ func (l *ArticleLogic) Article(in *web.ArticleReq) (*web.ArticleResp, error) {
 	//判断数据是否初始化
 	if len(article) > 0 {
 		// mongodb获取文章内容
-		conn := model.NewArticleModel(l.svcCtx.MongoUri, l.svcCtx.Config.MongoDB.DATABASE, "txy_article")
-		contentId, ok := article["_id"].([]byte)
-		if !ok {
-			// 处理类型断言失败的情况
-			return nil, errors.New("content_id is not a string")
-		}
-		if string(contentId) != "" {
-			res, err := conn.FindOne(l.ctx, string(contentId))
-			if err != nil {
-				if err != mongo.ErrNoDocuments && err.Error() != "invalid objectId" {
-					logc.Errorf(l.ctx, "Document not found or invalid ObjectId: %s", err)
-					// 对于其他类型的错误，仍然返回
-					return nil, err
-				}
-			} else {
-				article["content"] = res.Content
-			}
-		}
+		//conn := model.NewArticleModel(l.svcCtx.MongoUri, l.svcCtx.Config.MongoDB.DATABASE, "txy_article")
+		//contentId, ok := article["title"].(string)
+		//if !ok {
+		//	// 处理类型断言失败的情况
+		//	return nil, errors.New("content_id is not a string")
+		//}
+		//res, err := conn.FindOne(l.ctx, contentId)
+		//if err != nil {
+		//	return nil, err
+		//}
+		//article["content"] = res.Content
 	}
-	// 转换 _id 字段的类型
-	if idBytes, ok := article["_id"].([]byte); ok {
-		article["_id"] = string(idBytes)
+	// redis实例
+	err = l.svcCtx.Rds.SetCtx(l.ctx, "key", "hello world")
+	if err != nil {
+		logc.Error(l.ctx, err)
 	}
+	v, err := l.svcCtx.Rds.GetCtx(l.ctx, "key")
+	if err != nil {
+		logc.Error(l.ctx, err)
+	}
+	article["v"] = v
 	jsonData, err := json.Marshal(article)
 	if err != nil {
 		return nil, err
