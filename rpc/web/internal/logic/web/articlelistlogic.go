dac036cfe074cfe4f7aef85349059342e785470e
leixiaotian <tian-lei-happy@163.com>
2024-12-15 12:59:30 +0800

    fxi:关键词搜索

--- rpc/web/internal/logic/web/articlelistlogic.go
+++ rpc/web/internal/logic/web/articlelistlogic.go
@@ -28,6 +28,7 @@ func NewArticleListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Artic
 
 func (l *ArticleListLogic) ArticleList(in *web.ArticleListReq) (*web.ArticleListResp, error) {
 	where := map[string]interface{}{}
+	db := l.svcCtx.DB.Model(&mysql.TxyArticle{})
 	order := "id desc"
 	if in.Cid > 0 {
 		where["cid"] = in.Cid
@@ -46,7 +47,7 @@ func (l *ArticleListLogic) ArticleList(in *web.ArticleListReq) (*web.ArticleList
 		}
 	}
 	if in.Keywords != "" {
-		where["title like"] = "%" + in.Keywords + "%"
+		db = db.Where("title like ?", "%"+in.Keywords+"%")
 	}
 	if in.Page == 0 {
 		in.Page = 1
@@ -56,9 +57,7 @@ func (l *ArticleListLogic) ArticleList(in *web.ArticleListReq) (*web.ArticleList
 	}
 	offset := (in.Page - 1) * in.PageSize
 	var articles []map[string]interface{}
-	err := l.svcCtx.DB.
-		Model(&mysql.TxyArticle{}).
-		Select("txy_article.id,txy_article.path,txy_article.title,txy_article.author,txy_article.description,txy_article.keywords,txy_article.cid,txy_article.tid,txy_article.created_at,txy_article.view_count,c.name cname,t.name tname").
+	err := db.Select("txy_article.id,txy_article.path,txy_article.title,txy_article.author,txy_article.description,txy_article.keywords,txy_article.cid,txy_article.tid,txy_article.created_at,txy_article.view_count,c.name cname,t.name tname").
 		Joins("left join txy_category as c on txy_article.cid = c.id").
 		Joins("left join txy_tag as t on txy_article.tid = t.id").
 		Where(where).
@@ -76,7 +75,7 @@ func (l *ArticleListLogic) ArticleList(in *web.ArticleListReq) (*web.ArticleList
 	}
 	//计算当前type的总数，给分页算总页
 	var total int64
-	err = l.svcCtx.DB.Model(&mysql.TxyArticle{}).Where(where).Count(&total).Error
+	err = db.Where(where).Count(&total).Error
 	if err != nil {
 		return nil, err
 	}
