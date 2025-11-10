f179b7d4f87e98a9aaa8f20fe234b96a7c04d4f4
leixiaotian <tian-lei-happy@163.com>
2025-11-10 22:49:35 +0800

    fix:debug pay

--- gateway/internal/handler/payment/paymentnotifyhandler.go
+++ gateway/internal/handler/payment/paymentnotifyhandler.go
@@ -1,18 +1,33 @@
 package payment
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 
-	"github.com/zeromicro/go-zero/rest/httpx"
 	"lxtian-blog/gateway/internal/logic/payment"
 	"lxtian-blog/gateway/internal/svc"
 	"lxtian-blog/gateway/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 // 支付结果异步通知
 func PaymentNotifyHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.PaymentNotifyReq
+
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			logx.Errorf("PaymentNotifyHandler read body error: %v", err)
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		logx.Infof("PaymentNotifyHandler request: method=%s url=%s header=%v query=%v body=%s", r.Method, r.URL.String(), r.Header, r.URL.Query(), string(bodyBytes))
+
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 			return
